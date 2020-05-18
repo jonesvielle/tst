b46a78ae9d6f04b6c09f6fb5f367ee05b6bf0343
jones obenobe <obenobejones@gmail.com>
2020-05-18 23:20:07 +0100

    adding starting project

---
+++ app.js
@@ -0,0 +1,27 @@
+// function isUserValid(bool){
+//     return bool;
+// }
+
+// var answer = isUserValid(true) ? "you may enter" : "Access Granted";
+// const name = 'john snow';
+// const obj = {
+//     [name]: 'hello',
+//     [1+2]: 'smart'
+// }
+// const name = "sally";
+// const age = 24;
+// const pet = "dog";
+// const greetingBest = `Hello ${name} you seem to be ${age-10}. What a lovely ${pet} you have`;
+ 
+// function greet(name='', age=38, pet='cat')
+// {
+//     return `Hello ${name} you seem to be ${age-10}. What a lovely ${pet} you have`;
+// }
+
+// let sym1 = Symbol();
+// let sym2 = Symbol('foo');
+// let sym2 = Symbol('foo');
+
+const object = {
+    name: 'sally'
+}
